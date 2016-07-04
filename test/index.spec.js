b12f78afe258b925a9be016c931c78e29348689a
Seb Insua <me@sebinsua.com>
2016-07-04 13:02:32 +0100

    fix(es6): handle es6 imports and callable webpacks

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,54 +1,54 @@
-
-const babel = require('babel-core');
 import test from 'ava';
+
+import { transformFileSync } from 'babel-core';
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
 function transformFile(path, configuration = { config: './runtime.webpack.config.js' }) {
-    return babel.transformFileSync(resolve(__dirname, path), {
+    const out = transformFileSync(resolve(__dirname, path), {
         plugins: [
-            ['../../src/index.js', configuration]
+            [ '../../src/index.js', configuration ]
         ]
     });
+    return out && out.code ? out.code.trim() : '';
 }
 
 function read(path) {
-    return readFileSync(resolve(__dirname, path), 'utf8');
+    return readFileSync(resolve(__dirname, path), 'utf8').trim();
 }
 
 test('basic require with the absolute resolve path', t => {
-    const actual = transformFile('fixtures/basic.absolute.js', {config: './runtime.webpack.config.js'}).code;
+    const actual = transformFile('fixtures/basic.absolute.js', { config: './runtime.webpack.config.js' });
     const expected = read('fixtures/basic.expected.js');
     t.is(actual, expected);
 });
 
 test('basic require with the relative resolve path', t => {
-    const actual = transformFile('fixtures/basic.relative.js', {config: './runtime.webpack.config.js'}).code;
+    const actual = transformFile('fixtures/basic.relative.js', { config: './runtime.webpack.config.js' });
     const expected = read('fixtures/basic.expected.js');
     t.is(actual, expected);
 });
 
 test('filename require', t => {
-    const actual = transformFile('fixtures/filename.js', {config: './runtime.webpack.config.js'}).code;
+    const actual = transformFile('fixtures/filename.js', { config: './runtime.webpack.config.js' });
     const expected = read('fixtures/filename.expected.js');
     t.is(actual, expected);
 });
 
 test('variable assignment', t => {
-    const actual = transformFile('fixtures/variables.js', {config: './runtime.webpack.config.js'}).code;
+    const actual = transformFile('fixtures/variables.js', { config: './runtime.webpack.config.js' });
     const expected = read('fixtures/variables.expected.js');
     t.is(actual, expected);
 });
 
 test('requiring files from the root', t => {
-    const actual = transformFile('fixtures/rootfolder.js', {config: './runtime.webpack.config.js'}).code;
+    const actual = transformFile('fixtures/rootfolder.js', { config: './runtime.webpack.config.js' });
     const expected = read('fixtures/rootfolder.expected.js');
     t.is(actual, expected);
 });
 
-
 test('using the import syntax', t => {
-    const actual = transformFile('fixtures/import.js', {config: './runtime.webpack.config.js'}).code;
+    const actual = transformFile('fixtures/import.js', { config: './runtime.webpack.config.js' });
     const expected = read('fixtures/import.expected.js');
 
     t.is(actual, expected);
